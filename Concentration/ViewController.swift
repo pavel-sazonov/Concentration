8f6eeeb3c09c4d60e83dd2483e1b645aaf5ae1ce
pavel-sazonov <wismyl@ya.ru>
2019-02-15 17:44:00 +0300

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Pavel Sazonov on 15/02/2019.
+//  Copyright © 2019 Pavel Sazonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
