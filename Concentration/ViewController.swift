98e7fa8ffb28ded0260960b16ee4ee37b47aa9cc
pavel-sazonov <wismyl@ya.ru>
2019-02-16 11:25:49 +0300

    second card

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
         flipCard(withEmoji: "👻", on: sender)
     }
     
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🎃", on: sender)
+    }
+    
     func flipCard(withEmoji emoji: String, on button: UIButton) {
         if button.currentTitle == emoji {
             button.setTitle("", for: UIControl.State.normal)
